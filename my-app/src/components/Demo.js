dabd0ad5de5303fe5358939697b54f1b9a69dc63
vicky7407 <vinayrathod5475@gmil.com>
2022-05-27 15:40:16 +0530

    rcc

---
+++ my-app/src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class  Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello my name is Vinay</h1>
+            </div>
+        );
+    }
+}
+
+export default Demo;
