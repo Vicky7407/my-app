1acdbc0213fc89df6856afa4451fc0193eca9ad5
vicky7407 <vinayrathod5475@gmil.com>
2022-06-14 15:24:19 +0530

    hoc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,32 @@ import Country from './Countainer/Country';
 import Time from './Time/Time';
 import Timefun from './Time/Timefun';
 import Count from './Countainer/Counter';
+import Loading from './Countainer/Loading';
+import Home from './Countainer/Home';
+import { useEffect, useState } from 'react';
 
-function App (){
-   return(
+const HomeWithLoading = Loading(Home)
+
+function App(props) {
+  const [Loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  let orgData = [
+    { id: 101, name: "Vinay" },
+    { id: 102, name: "Gaurav" }
+  ];
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => { setLoading(false); setData(orgData) }, 2000);
+  }, [])
+  return (
     <div>
-       {/* <Time/> */}
-       {/* <Timefun /> */}
-       < Count/>
-       
+      <HomeWithLoading
+        isLoading={Loading}
+        data={data} 
+        />
     </div>
-   )
+  );
 }
-{/* <Country/> */}
-
-
-
-
 
 export default App;
